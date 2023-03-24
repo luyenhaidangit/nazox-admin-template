01ac06f597a07ea061f673c2912fd35a4d18200a
luyenhaidangit <106638147+luyenhaidangit@users.noreply.github.com>
2023-03-24 18:30:22 +0700

    Add layout project

--- src/layouts/RightSideBar.js
+++ src/layouts/RightSideBar.js
@@ -5,9 +5,9 @@ const RightSideBar = () => {
         <div class="right-bar">
             <div data-simplebar class="h-100">
                 <div class="rightbar-title px-3 py-4">
-                    <a href="javascript:void(0);" class="right-bar-toggle float-right">
+                    <span class="right-bar-toggle float-right">
                         <i class="mdi mdi-close noti-icon"></i>
-                    </a>
+                    </span>
                     <h5 class="m-0">Settings</h5>
                 </div>
 
