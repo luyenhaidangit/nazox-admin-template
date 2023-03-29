b15f80c885014ebaf568d4778bc02db686990656
luyenhaidangit <106638147+luyenhaidangit@users.noreply.github.com>
2023-03-29 18:06:45 +0700

    Create template delete multi product category

--- src/pages/ProductCategory/ProductCategoy.js
+++ src/pages/ProductCategory/ProductCategoy.js
@@ -1,27 +1,46 @@
-import React, { useEffect, useState } from 'react'
-import { useTable, useFilters, useGlobalFilter, useAsyncDebounce, usePagination } from 'react-table'
+import React, { useEffect, useState, useRef } from 'react'
 import { GetProductCategoryManage } from '../../apis/productCategoryApiService';
-import CreatableSelect from 'react-select/creatable';
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 import "../../assets/libs/datatables.net/css/jquery.dataTables.css"
 import "../../assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css"
 import "../../assets/libs/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css"
 import "../../assets/libs/datatables.net-select-bs4/css/select.bootstrap4.min.css"
 import "../../assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css"
-import { FaSortUp, FaSortDown } from 'react-icons/fa';
-
-
-// React select
-import Select, { components } from 'react-select';
-import makeAnimated from 'react-select/animated';
 
 const ProductCategoy = () => {
     const [productCategories, setProductCategories] = useState([]);
     const [pageIndex, setPageIndex] = useState(1);
     const [pageSize, setPageSize] = useState(10);
     const [totalPages, setTotalPages] = useState(0);
-    const [sortOrder, setSortOrder] = useState(null);
     const [sortState, setSortState] = useState({ sortBy: null, orderBy: null });
+    const [selectedRecords, setSelectedRecords] = useState([]);
+    const selectAllCheckboxRef = useRef();
 
+    // Handle Page
+    useEffect(() => {
+        const request = {
+            pageIndex,
+            pageSize
+        }
+        fetchProductCateogories(request);
+    }, []);
+
+    const fetchProductCateogories = async (request) => {
+        let res = await GetProductCategoryManage(request);
+        setProductCategories(res.items);
+        setTotalPages(res.totalPages);
+    }
+
+    useEffect(() => {
+        const request = {
+            pageIndex: pageIndex,
+            pageSize: pageSize,
+        }
+        fetchProductCateogories(request);
+    }, [pageIndex]);
+
+    // Handle Sort
     const handleSort = (sortBy) => {
         if (sortState.sortBy === sortBy) {
             setSortState({
@@ -35,46 +54,76 @@ const ProductCategoy = () => {
 
     useEffect(() => {
         const request = {
-            pageIndex,
-            pageSize
-        }
-        fetchProductCateogories(request);
-    }, []);
-
-    const changePageIndex = (index) => {
-        setPageIndex(index);
-    }
-
-    useEffect(() => {
-        const request = {
-            pageIndex: pageIndex,
-            pageSize: pageSize,
-        }
-        fetchProductCateogories(request);
-    }, [pageIndex]);
-
-    useEffect(() => {
-        const request = {
             pageIndex: pageIndex,
             pageSize: pageSize,
             sortBy: sortState.sortBy,
             orderBy: sortState.orderBy
         };
-        console.log(request)
-        // Call your API here with the updated request object
         fetchProductCateogories(request);
     }, [sortState]);
 
-    const fetchProductCateogories = async (request) => {
-        let res = await GetProductCategoryManage(request);
-        setProductCategories(res.items);
-        setTotalPages(res.totalPages);
+    // Handel Multi
+    const handleSelectAllCheckbox = (event) => {
+        const isChecked = event.target.checked;
+        let selectedIds = [];
+        if (isChecked) {
+            selectedIds = [...selectedRecords, ...productCategories.map(record => record.id)];
+            selectedIds = [...new Set(selectedIds)];
+            selectAllCheckboxRef.current.indeterminate = false;
+        } else {
+            const currentPageIds = productCategories.map(record => record.id);
+            selectedIds = selectedRecords.filter(id => !currentPageIds.includes(id));
+        }
+        setSelectedRecords(selectedIds);
+    };
+
+    const handleSelectRecord = (event, record) => {
+        const recordId = record.id;
+        const isSelected = event.target.checked;
+        const selectedIds = [...selectedRecords];
+
+        if (isSelected) {
+            selectedIds.push(recordId);
+        } else {
+            const index = selectedIds.indexOf(recordId);
+            selectedIds.splice(index, 1);
+        }
+        setSelectedRecords(selectedIds);
+
+        const currentPageIds = productCategories.map(record => record.id);
+        selectAllCheckboxRef.current.indeterminate = selectedIds.some(id => currentPageIds.includes(id));
+    };
+
+    const submitDeleteMulti = () => {
+        const MySwal = withReactContent(Swal)
+
+        MySwal.fire({
+            title: `Bạn có chắc muốn xóa ${selectedRecords.length} bản ghi?`,
+            text: "Bạn sẽ không thể khôi phục dữ liệu sau khi xóa!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Xác nhận',
+            cancelButtonText: 'Bỏ qua'
+        }).then((result) => {
+            // Api delete multi
+
+            if (result.isConfirmed) {
+                Swal.fire(
+                    'Đã xóa!',
+                    'Dữ liệu đã xóa khỏi hệ thống!.',
+                    'success'
+                )
+            }
+        })
     }
 
     return (
         <>
             <div className="row">
                 <div className="col-12">
+
                     <div className="page-title-box d-flex align-items-center justify-content-between">
                         <h4 className="mb-0">Loại sản phẩm</h4>
 
@@ -93,9 +142,12 @@ const ProductCategoy = () => {
                 <div className="col-12">
                     <div className="card">
                         <div className="card-body">
-                            {/* <h3 className="card-title">Quản lý loại sản phẩm</h3> */}
                             <div className='group-btn'>
                                 <button type="button" className="btn btn-success waves-effect waves-light mt-2 mb-3 mr-2">Thêm mới</button>
+                                {
+                                    selectedRecords && selectedRecords.length > 0 &&
+                                    <button onClick={() => submitDeleteMulti()} type="button" className="btn btn-danger waves-effect waves-light mt-2 mb-3 mr-2">Xóa {selectedRecords.length} lựa chọn</button>
+                                }
                             </div>
                             <div className="row">
                                 <div className="col-lg-12 d-flex align-items-center">
@@ -111,6 +163,18 @@ const ProductCategoy = () => {
                                 <table id="datatable" class="dataTable table table-bordered dt-responsive nowrap" style={{ borderCollapse: 'collapse', borderSpacing: '0', width: '100%', margin: "0 12px" }}>
                                     <thead>
                                         <tr>
+                                            <th style={{ maxWidth: "4px", borderRight: "none" }}>
+                                                <div class="custom-control custom-checkbox" style={{ width: "6px" }}>
+                                                    <input
+                                                        type="checkbox"
+                                                        class="custom-control-input"
+                                                        id="ordercheck"
+                                                        onChange={(e) => handleSelectAllCheckbox(e)}
+                                                        checked={productCategories.map((item) => item.id).every((item) => selectedRecords.includes(item))}
+                                                        ref={selectAllCheckboxRef} />
+                                                    <label class="custom-control-label" for="ordercheck">&nbsp;</label>
+                                                </div>
+                                            </th>
                                             <th className='d-flex align-items-center sorting_asc' onClick={() => handleSort('id')}>
                                                 #
                                             </th>
@@ -129,7 +193,13 @@ const ProductCategoy = () => {
                                             productCategories && productCategories.map((item, index) => {
                                                 return (
                                                     <tr>
-                                                        <td>{item.id}</td>
+                                                        <td style={{ maxWidth: "4px", borderRight: "none" }}>
+                                                            <div class="custom-control custom-checkbox" style={{ width: "6px" }}>
+                                                                <input type="checkbox" class="custom-control-input" id={`ordercheck-${item.id}`} checked={selectedRecords.includes(item.id)} onChange={(event) => handleSelectRecord(event, item)} />
+                                                                <label class="custom-control-label" for={`ordercheck-${item.id}`}>&nbsp;</label>
+                                                            </div>
+                                                        </td>
+                                                        <td style={{ paddingLeft: "0px" }}>{item.id}</td>
                                                         <td>{item.name}</td>
                                                         <td className='d-flex justify-content-center'>
                                                             {
