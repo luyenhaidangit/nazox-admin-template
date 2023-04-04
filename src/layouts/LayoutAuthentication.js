53d88d7d70da47c2f4bac297e952638220b6965b
luyenhaidangit <106638147+luyenhaidangit@users.noreply.github.com>
2023-04-04 20:16:30 +0700

    Clean code login page

--- src/layouts/LayoutAuthentication.js
+++ src/layouts/LayoutAuthentication.js
@@ -10,17 +10,17 @@ const LayoutAuthentication = () => {
             <link href="../assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
             <link href="../assets/css/custom.css" rel="stylesheet" type="text/css" />
 
-            <div class="home-btn d-none d-sm-block">
-                <a href="index.html"><i class="mdi mdi-home-variant h2 text-white"></i></a>
+            <div className="home-btn d-none d-sm-block">
+                <a href="index.html"><i className="mdi mdi-home-variant h2 text-white"></i></a>
             </div>
             <div>
-                <div class="container-fluid p-0">
-                    <div class="row no-gutters">
-                        <div class="col-lg-6">
-                            <div class="authentication-page-content p-4 d-flex align-items-center min-vh-100">
-                                <div class="w-100">
-                                    <div class="row justify-content-center">
-                                        <div class="col-lg-7">
+                <div className="container-fluid p-0">
+                    <div className="row no-gutters">
+                        <div className="col-lg-6">
+                            <div className="authentication-page-content p-4 d-flex align-items-center min-vh-100">
+                                <div className="w-100">
+                                    <div className="row justify-content-center">
+                                        <div className="col-lg-7">
 
                                             <Outlet />
                                         </div>
@@ -28,9 +28,9 @@ const LayoutAuthentication = () => {
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-6">
-                            <div class="authentication-bg d-flex align-items-center">
-                                {/* <div class="bg-overlay"></div> */}
+                        <div className="col-lg-6">
+                            <div className="authentication-bg d-flex align-items-center">
+                                {/* <div className="bg-overlay"></div> */}
                                 <img className='img-fluid' style={{ height: "100%", width: "100%" }} src="/assets/images/thegioididong.jpg" alt="logo" />
                             </div>
                         </div>
--- src/pages/Authentication/Login.js
+++ src/pages/Authentication/Login.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react'
 import { Authentication } from '../../apis/authenticationApiService'
-import { ToastContainer, toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 import { useNavigate } from "react-router-dom";
-
 import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
@@ -19,11 +18,47 @@ const Login = () => {
     const handleLogin = async (event) => {
         event.preventDefault(); // ngăn chặn submit form mặc định
 
-        let res = await Authentication(userName, password);
-        console.log(res)
-        if (res.statusCode === 200) {
-            localStorage.setItem('token', res?.data?.token);
-            toast.success(res.message, {
+        try {
+            let res = await Authentication(userName, password);
+            console.log(res)
+            if (res.statusCode === 200) {
+                localStorage.setItem('token', res?.data?.token);
+                toast.success(res.message, {
+                    position: toast.POSITION.TOP_RIGHT, // Vị trí hiển thị của thông báo
+                    autoClose: 3000, // Thời gian tự động đóng thông báo (đơn vị là miliseconds)
+                    hideProgressBar: false, // Ẩn hoặc hiển thị thanh tiến trình
+                    closeOnClick: true, // Đóng thông báo khi click vào nó
+                    pauseOnHover: true, // Tạm dừng thời gian tự động đóng khi hover chuột vào thông báo
+                    draggable: true, // Cho phép kéo thông báo
+                    progress: undefined // Không sử dụng thanh tiến trình tích lũy
+                });
+
+                navigate("/");
+            }
+
+            else if (res?.statusCode === 401) {
+                toast.error(res.message, {
+                    position: toast.POSITION.TOP_RIGHT, // Vị trí hiển thị của thông báo
+                    autoClose: 3000, // Thời gian tự động đóng thông báo (đơn vị là miliseconds)
+                    hideProgressBar: false, // Ẩn hoặc hiển thị thanh tiến trình
+                    closeOnClick: true, // Đóng thông báo khi click vào nó
+                    pauseOnHover: true, // Tạm dừng thời gian tự động đóng khi hover chuột vào thông báo
+                    draggable: true, // Cho phép kéo thông báo
+                    progress: undefined // Không sử dụng thanh tiến trình tích lũy
+                });
+            } else {
+                toast.error('Xuất hiện lỗi trong quá trình thực hiện!', {
+                    position: toast.POSITION.TOP_RIGHT, // Vị trí hiển thị của thông báo
+                    autoClose: 3000, // Thời gian tự động đóng thông báo (đơn vị là miliseconds)
+                    hideProgressBar: false, // Ẩn hoặc hiển thị thanh tiến trình
+                    closeOnClick: true, // Đóng thông báo khi click vào nó
+                    pauseOnHover: true, // Tạm dừng thời gian tự động đóng khi hover chuột vào thông báo
+                    draggable: true, // Cho phép kéo thông báo
+                    progress: undefined // Không sử dụng thanh tiến trình tích lũy
+                });
+            }
+        } catch (e) {
+            toast.error('Xuất hiện lỗi trong quá trình thực hiện!', {
                 position: toast.POSITION.TOP_RIGHT, // Vị trí hiển thị của thông báo
                 autoClose: 3000, // Thời gian tự động đóng thông báo (đơn vị là miliseconds)
                 hideProgressBar: false, // Ẩn hoặc hiển thị thanh tiến trình
@@ -32,21 +67,9 @@ const Login = () => {
                 draggable: true, // Cho phép kéo thông báo
                 progress: undefined // Không sử dụng thanh tiến trình tích lũy
             });
-
-            navigate("/");
+            console.log(e)
         }
 
-        else {
-            toast.error(res.message, {
-                position: toast.POSITION.TOP_RIGHT, // Vị trí hiển thị của thông báo
-                autoClose: 3000, // Thời gian tự động đóng thông báo (đơn vị là miliseconds)
-                hideProgressBar: false, // Ẩn hoặc hiển thị thanh tiến trình
-                closeOnClick: true, // Đóng thông báo khi click vào nó
-                pauseOnHover: true, // Tạm dừng thời gian tự động đóng khi hover chuột vào thông báo
-                draggable: true, // Cho phép kéo thông báo
-                progress: undefined // Không sử dụng thanh tiến trình tích lũy
-            });
-        }
     };
 
     const handleUsernameChange = (event) => {
@@ -60,47 +83,47 @@ const Login = () => {
     return (
         <>
             <div>
-                <div class="text-center">
+                <div className="text-center">
                     <div>
-                        <a href="index.html" class="logo"><img src="/assets/images/logo-dagstore.png" height="32" alt="logo" /></a>
+                        <a href="index.html" className="logo"><img src="/assets/images/logo-dagstore.png" height="32" alt="logo" /></a>
                     </div>
 
-                    <h4 class="font-size-18 mt-4">Xin chào !</h4>
-                    <p class="text-muted">Đăng nhập để tiếp tục truy cập hệ thống Thegioididong!</p>
+                    <h4 className="font-size-18 mt-4">Xin chào !</h4>
+                    <p className="text-muted">Đăng nhập để tiếp tục truy cập hệ thống Thegioididong!</p>
                 </div>
 
-                <div class="p-2 mt-5">
-                    <form class="form-horizontal" onSubmit={(e) => handleLogin(e)}>
+                <div className="p-2 mt-5">
+                    <form className="form-horizontal" onSubmit={(e) => handleLogin(e)}>
 
-                        <div class="form-group auth-form-group-custom mb-4">
-                            <i class="ri-user-2-line auti-custom-input-icon"></i>
-                            <label for="username">Tên tài khoản</label>
-                            <input value={userName} onChange={(e) => handleUsernameChange(e)} type="text" class="form-control" id="username" placeholder="Enter username" />
+                        <div className="form-group auth-form-group-custom mb-4">
+                            <i className="ri-user-2-line auti-custom-input-icon"></i>
+                            <label htmlFor="username">Tên tài khoản</label>
+                            <input value={userName} onChange={(e) => handleUsernameChange(e)} type="text" className="form-control" id="username" placeholder="Enter username" />
                         </div>
 
-                        <div class="form-group auth-form-group-custom mb-4">
-                            <i class="ri-lock-2-line auti-custom-input-icon"></i>
-                            <label for="userpassword">Mật khẩu</label>
-                            <input value={password} onChange={(e) => handlePasswordChange(e)} type="password" class="form-control" id="userpassword" placeholder="Enter password" />
+                        <div className="form-group auth-form-group-custom mb-4">
+                            <i className="ri-lock-2-line auti-custom-input-icon"></i>
+                            <label htmlFor="userpassword">Mật khẩu</label>
+                            <input value={password} onChange={(e) => handlePasswordChange(e)} type="password" className="form-control" id="userpassword" placeholder="Enter password" />
                         </div>
 
-                        <div class="custom-control custom-checkbox">
-                            <input type="checkbox" class="custom-control-input" id="customControlInline" />
-                            <label class="custom-control-label" for="customControlInline">Lưu đăng nhập</label>
+                        <div className="custom-control custom-checkbox">
+                            <input type="checkbox" className="custom-control-input" id="customControlInline" />
+                            <label className="custom-control-label" htmlFor="customControlInline">Lưu đăng nhập</label>
                         </div>
 
-                        <div class="mt-4 text-center">
-                            <button class="btn btn-primary w-md waves-effect waves-light" type="submit">Đăng nhập</button>
+                        <div className="mt-4 text-center">
+                            <button className="btn btn-primary w-md waves-effect waves-light" type="submit">Đăng nhập</button>
                         </div>
 
-                        <div class="mt-4 text-center">
-                            <a href="auth-recoverpw.html" class="text-muted"><i class="mdi mdi-lock mr-1"></i>Quên mật khẩu?</a>
+                        <div className="mt-4 text-center">
+                            <a href="auth-recoverpw.html" className="text-muted"><i className="mdi mdi-lock mr-1"></i>Quên mật khẩu?</a>
                         </div>
                     </form>
                 </div>
 
-                <div class="mt-5 text-center">
-                    <p>© 2023 FIT.UTEHY <i class="mdi mdi-heart text-danger"></i></p>
+                <div className="mt-5 text-center">
+                    <p>© 2023 FIT.UTEHY <i className="mdi mdi-heart text-danger"></i></p>
                 </div>
             </div>
         </>
